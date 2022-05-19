d1003323449905159f107148282d9a377cc78dcb
yangjingsheng <yangjingsheng8863@dingtalk.com>
2022-05-19 14:46:12 +0800

    init project

---
+++ MountainAndSeaServer/base-core/src/main/java/com/mountain.sea/App.java
@@ -0,0 +1,13 @@
+package com.mountain.sea;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
