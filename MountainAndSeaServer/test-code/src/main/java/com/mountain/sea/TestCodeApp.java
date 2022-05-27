4e3dad2a309189f8cbca04e9b46be2a0a30cc385
yangjingsheng <yangjingsheng8863@dingtalk.com>
2022-05-27 15:42:23 +0800

    启动配置文件调整解决nacos配置中心不生效问题

--- MountainAndSeaServer/test-code/src/main/java/com/mountain/sea/TestCodeApp.java
+++ MountainAndSeaServer/test-code/src/main/java/com/mountain/sea/TestCodeApp.java
@@ -4,8 +4,13 @@ import com.mountain.sea.core.context.feign.EnableFeignTargeter;
 import com.mountain.sea.core.context.feign.EnableSentinelFeignRewrite;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 /**
  * Hello world!
@@ -14,11 +19,18 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @EnableFeignClients
 @EnableSentinelFeignRewrite
+@EnableDiscoveryClient
 public class TestCodeApp
 {
     public static void main( String[] args )
     {
         SpringApplication.run(TestCodeApp.class,args);
         System.out.println( "Hello World!" );
+    }
+
+    @LoadBalanced
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
--- MountainAndSeaServer/test-code/src/main/java/com/mountain/sea/controller/TestController.java
+++ MountainAndSeaServer/test-code/src/main/java/com/mountain/sea/controller/TestController.java
@@ -4,6 +4,8 @@ import com.alibaba.csp.sentinel.annotation.SentinelResource;
 import com.mountain.sea.microservice.TestFeign;
 import com.mountain.sea.microservice.TestFeign2;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,16 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
  * @date 2022/5/20 16:37
  */
 @RestController
+@RefreshScope
 public class TestController {
     @Autowired
     TestFeign testFeign;
     @Autowired
     TestFeign2 testFeign2;
+    @Value("${nacos.test.data:abc}")
+    String testData;
 
     @RequestMapping("/test")
     @SentinelResource("/test")
     public Object a(){
 //        throw new RestApiException("100","test");
+        System.out.println(testData);
         return "hello world";
     }
 
